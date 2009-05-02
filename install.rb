5f6093ae7f0f19055a3528989abba408f9ce6edf
Sai Emrys <github@saizai.com>
2009-05-02 00:41:03 -0700

    better install hook; cleaned up example deploy file

--- install.rb
+++ install.rb
@@ -1,11 +1,18 @@
+require 'capistrano'
 require 'fileutils'
 
 example_deploy = File.join(File.dirname(__FILE__), 'deploy.rb.example')
-destination_file = File.join(RAILS_ROOT, 'config', 'deploy.rb.example')
+real_deploy = File.join(RAILS_ROOT, 'config', 'deploy.rb')
+real_example = File.join(RAILS_ROOT, 'config', 'deploy.rb.example')
 
-if File.exists? destination_file
-  puts "You already have a config/deploy.rb.example file..."
-else
-  FileUtils.cp example_deploy, destination_file
+if !File.exists? real_deploy
+  FileUtils.cp example_deploy, real_deploy
+  puts "SuperDeploy installed. Please edit config/deploy.rb"
+elsif !File.exists? real_example
+  FileUtils.cp example_deploy, real_example
   puts "SuperDeploy installed. Please edit config/deploy.rb.example"
+else
+  puts "You already have a config/deploy.rb.example file..."
 end
+
+exec "capify #{RAILS_ROOT}"
