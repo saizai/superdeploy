02dd77fae9f752d8fc2ea32a644246b1353d673e
Matteo Latini <mtylty@gmail.com>
2011-05-25 14:37:05 +0200

    moves bundler requirements to Capfile

--- install.rb
+++ install.rb
@@ -4,6 +4,9 @@ require 'fileutils'
 example_deploy = File.join(File.dirname(__FILE__), 'deploy.rb.example')
 real_deploy = File.join(RAILS_ROOT, 'config', 'deploy.rb')
 real_example = File.join(RAILS_ROOT, 'config', 'deploy.rb.example')
+gemfile = File.join(RAILS_ROOT, 'Gemfile')
+
+capify_cmd = "capify #{RAILS_ROOT}"
 
 if !File.exists? real_deploy
   FileUtils.cp example_deploy, real_deploy
@@ -15,4 +18,8 @@ else
   puts "You already have both config/deploy.rb and config/deploy.rb.example file..."
 end
 
-exec "capify #{RAILS_ROOT}"
+if system(capify_cmd) && File.exists?(gemfile)
+  exec 'echo "require \'bundler/capistrano\'" >> Capfile'
+else
+  exec 'echo "# require \'bundler/capistrano\' # Uncomment this in case you want to use bundler deploy tasks" >> Capfile'
+end
